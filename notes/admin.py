099c578d1fd33b0e8ad8b63ad1a3d1519acdcd38
Frank Wickström <frank@bambuser.com>
2015-05-04 17:16:40 +0300

    Get GenericTabularInline from contrib.contenttypes.admin
    .generic has been deprecated and will be removed with Django 1.9
    deprecated in Django 1.7

--- notes/admin.py
+++ notes/admin.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from notes.models import Note
 from django.contrib import admin
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes import admin as ct_admin
 
 
-class NoteInline(generic.GenericTabularInline):
+class NoteInline(ct_admin.GenericTabularInline):
     model = Note
 
 admin.site.register(Note)
